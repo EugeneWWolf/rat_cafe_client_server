02cca8241e3ce3a61fbdeb7252f2748ecf0a5f08
Cody <tortoisegames431@gmail.com>
2024-04-21 17:10:44 +0300

    Add Food entity for PostgreSQL database, configured TypeORM to work with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FoodModule } from './food/food.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Food } from './food/entities/food.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1488',
+      database: 'rat_cafe',
+      entities: [Food],
+      synchronize: true,
+  }),
+  FoodModule,
+],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/food/entities/food.entity.ts
@@ -0,0 +1,32 @@
+import { Check, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum FoodCategory {
+    COFFEE = "coffee",
+    SALAD = "salad",
+    SOUP = "soup",
+    DESSERT = "dessert"
+}
+
+@Entity()
+@Check('"price" >= 100')
+export class Food {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({
+        type: "enum",
+        enum: FoodCategory
+    })
+    type: FoodCategory;
+
+    @Column()
+    price: number;
+
+    @Column({
+        nullable: true
+    })
+    description: string;
+}
