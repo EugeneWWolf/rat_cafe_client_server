3567e40cefefafd48298123d87c631200b6b8482
Cody <tortoisegames431@gmail.com>
2024-04-22 21:54:24 +0300

    Add more descriptive controller Http Exceptions

--- src/food/controllers/food/food.controller.ts
+++ src/food/controllers/food/food.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, NotFoundException, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CreateFoodDto } from 'src/food/dto/create-food.dto';
 import { UpdateFoodDto } from 'src/food/dto/update-food.dto';
 import { Food } from 'src/food/entities/food.entity';
 import { FoodService } from 'src/food/services/food/food.service';
+import { UpdateResult } from 'typeorm';
 
 /* 
     @TODO: Добавить проверку на поступающие данные (в виде пайпов?) на null, так как когда поступило null
@@ -20,34 +21,38 @@ export class FoodController {
 
     @Get()
     async findAll(): Promise<Food[]> {
-        return await this.foodService.findAll();
+        try {
+            return await this.foodService.findAll();
+        } catch(err) {
+            throw new NotFoundException("Database is empty");
+        }
     }
 
     @Get(':id')
     async findOne(@Param('id', ParseIntPipe) id: number): Promise<Food> {
-        return await this.foodService.findOne(id);
+        try {
+            return await this.foodService.findOne(id);
+        } catch(err) {
+            throw new NotFoundException(`Cannot find item with id ${id}`);
+        }
     }
 
     @Patch(':id')
-    async update(@Param('id', ParseIntPipe) id: number, @Body() updateFoodDto: UpdateFoodDto): Promise<void> {
+    async update(@Param('id', ParseIntPipe) id: number, @Body() updateFoodDto: UpdateFoodDto): Promise<UpdateResult> {
         try {
-            await this.foodService.update(id, updateFoodDto);
-        } catch(NotFoundException) {
-            throw new HttpException(`food with id "${id}" wasn't found`, HttpStatus.NOT_FOUND);
+            return await this.foodService.update(id, updateFoodDto);
+        } catch(err) {
+            throw new NotFoundException(`Cannot update item with id ${id}`);
         }
-    
-        return;
     }
 
     @Delete(':id')
-    async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    async remove(@Param('id', ParseIntPipe) id: number): Promise<Food> {
 
         try {
-            await this.foodService.remove(id);
-        } catch(NotFoundException) {
-            throw new HttpException(`food with id "${id}" wasn't found`, HttpStatus.NOT_FOUND);
+            return await this.foodService.remove(id);
+        } catch(err) {
+            throw new NotFoundException(`Cannot delete item with id ${id}`);
         }
-
-        return;
     }
 }
