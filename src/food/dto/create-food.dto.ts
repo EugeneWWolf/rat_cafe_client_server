cef137d49967b91e5a14f415166b0179578ff3b8
Cody <tortoisegames431@gmail.com>
2024-04-22 22:59:06 +0300

    Add Create-Food DTO validation (is not empty)  of obligatory fields

--- src/food/dto/create-food.dto.ts
+++ src/food/dto/create-food.dto.ts
@@ -1,8 +1,15 @@
+import { IsNotEmpty } from "class-validator";
 import { FoodCategory } from "../entities/food.entity";
 
 export class CreateFoodDto {
+    @IsNotEmpty({message: "Food name can't be empty"})
     name: string;
+
+    @IsNotEmpty({message: "Food category (type) can't be empty"})
     type: FoodCategory;
+
+    @IsNotEmpty({message: "Food price can't be empty"})
     price: number;
+
     description?: string;
 }
