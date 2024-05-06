a80062f9e6019d19a043ac486813f2fcb9e5f5f0
Cody <tortoisegames431@gmail.com>
2024-05-06 21:37:29 +0300

    Add additional checks for Product DTO, add more descriptive errors

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,19 +1,24 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsNumberString, IsOptional, IsString } from "class-validator";
 import { ProductCategory } from "../entities/product.entity";
 import { IsNotBlank } from "./custom_validators/isNotBlank";
 
 export class CreateProductDto {
-    @IsNotEmpty({message: "Food name can't be empty"})
-    @IsString()
+    @IsNotEmpty({message: "Product name can't be empty"})
+    @IsString({message: "Product name must be a string"})
+    @IsNotBlank('Check whether product name contains only whitespaces', {message: "Product name mustn't be blank (add at least 1 non-whitespace character)"})
     name: string;
 
-    @IsNotEmpty({message: "Food category (type) can't be empty"})
+    @IsNotEmpty({message: "Product category (type) can't be empty"})
+    @IsString({message: "Product type must be a string"})
+    @IsNotBlank('Check whether product category contains only whitespaces', {message: "Product category mustn't be blank (add at least 1 non-whitespace character)"})
     type: ProductCategory;
 
-    @IsNotEmpty({message: "Food price can't be empty"})
+    @IsNotEmpty({message: "Product price can't be empty"})
+    @IsNumberString({}, {message: "Product price must be a numeric string"})
     price: number;
 
     @IsOptional()
-    @IsNotBlank('Check if string contains not only whitespaces', {message: "String mustn't be blank (add at least 1 non-whitespace character)"})
+    @IsString({message: "Product description must be a string"})
+    @IsNotBlank('Check whether product description contains only whitespaces', {message: "Product description mustn't be blank (add at least 1 non-whitespace character)"})
     description?: string;
 }
