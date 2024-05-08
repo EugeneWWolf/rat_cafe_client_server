a70bd0f541b5da6505e8adec4aed88de7a29913e
Cody <tortoisegames431@gmail.com>
2024-05-08 18:05:35 +0300

    Remove Additional Service and Customer modules (for now) I need to make connections between tables first, don't wanna to rewrite a lot of code later

--- src/additional_service/dto/update-additional-service.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateAdditionalServiceDto } from "./create-additional-service.dto";
-
-export class UpdateAdditionalServiceDto extends PartialType(CreateAdditionalServiceDto) {}
