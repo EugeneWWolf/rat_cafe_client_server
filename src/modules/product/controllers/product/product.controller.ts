ddfce55580d7df35e89d0f2b505e471ec9d9b15f
Cody <tortoisegames431@gmail.com>
2024-05-10 18:31:07 +0300

    Working on exception filter, will rework code shortly in Product controller

--- src/modules/product/controllers/product/product.controller.ts
+++ src/modules/product/controllers/product/product.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Delete, Get, InternalServerErrorException, NotFoundException, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { BadRequestException, Body, ConflictException, Controller, Delete, Get, InternalServerErrorException, NotFoundException, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CreateProductDto } from 'src/modules/product/dto/create-product.dto';
 import { UpdateProductDto } from 'src/modules/product/dto/update-product.dto';
 import { Product } from 'src/modules/product/entities/product.entity';
 import { ProductService } from 'src/modules/product/services/product/product.service';
-import { NotFoundError } from 'src/utility/error_handling/database-errors';
+import { EntityNotFoundError, QueryFailedError } from 'typeorm';
 
 @Controller('product')
 export class ProductController {
@@ -14,7 +14,11 @@ export class ProductController {
         try {
             return await this.foodService.create(createProductDto);
         } catch (err) {
-            throw new InternalServerErrorException(err.message);
+            if (err instanceof QueryFailedError && err.message.includes('duplicate key value violates unique constraint')) {
+                throw new ConflictException(`Dublicate entry: product name must be unique`);
+            } else {
+                throw new InternalServerErrorException(`Failed to create entity`);
+            }
         }
     }
 
@@ -23,7 +27,7 @@ export class ProductController {
         try {
             return await this.foodService.findAll();
         } catch(err) {
-            throw new InternalServerErrorException(err.message);
+            throw new InternalServerErrorException(`Failed to fetch entities`);
         }
     }
 
@@ -32,7 +36,10 @@ export class ProductController {
         try {
             return await this.foodService.findOne(id);
         } catch(err) {
-            throw new NotFoundException(err.message);
+            if (err instanceof EntityNotFoundError) {
+                throw new NotFoundException(`Unable to find entity with ID ${id}`);
+            }
+            throw new InternalServerErrorException('Failed to fetch entity');
         }
     }
 
@@ -41,10 +48,12 @@ export class ProductController {
         try {
             await this.foodService.update(id, updateProductDto);
         } catch(err) {
-            if (err instanceof NotFoundError) {
-                throw new NotFoundException(err.message);
+            if (err instanceof EntityNotFoundError) {
+                throw new NotFoundException(`Unable to find entity with ID ${id}`);
+            } else if (err instanceof QueryFailedError && err.message.includes('duplicate key value violates unique constraint')) {
+                throw new ConflictException(`Dublicate entry (product name must be unique)`);
             } else {
-                throw new InternalServerErrorException(err.message);
+                throw new InternalServerErrorException(`Failed to update entity`);
             }
         }
     }
@@ -54,10 +63,10 @@ export class ProductController {
         try {
             return await this.foodService.remove(id);
         } catch(err) {
-            if (err instanceof NotFoundError) {
-                throw new NotFoundException(err.message);
+            if (err instanceof EntityNotFoundError) {
+                throw new NotFoundException(`Unable to find entity with ID ${id}`);
             } else {
-                throw new InternalServerErrorException(err.message);
+                throw new InternalServerErrorException(`Failed to delete entity`);
             }
         }
     }
