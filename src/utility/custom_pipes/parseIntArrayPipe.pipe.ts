7658088da0df2eb7f137165be6c87150ab2fbcf2
Cody <tortoisegames431@gmail.com>
2024-05-24 22:03:08 +0300

    Add Optional paramter to parseIntArrayPipe and improve error handling and stuff

--- src/utility/custom_pipes/parseIntArrayPipe.pipe.ts
+++ src/utility/custom_pipes/parseIntArrayPipe.pipe.ts
@@ -1,14 +1,34 @@
-import { Injectable, PipeTransform, BadRequestException } from "@nestjs/common";
+import { Injectable, PipeTransform, BadRequestException, Logger } from "@nestjs/common";
 
 @Injectable()
 export class ParseIntArrayPipe implements PipeTransform {
+  isOptional: boolean = false;
+
+  constructor(isOptional?: boolean) {
+    if (isOptional) {
+      this.isOptional = isOptional;
+    }
+  }
+
   transform(value: string): number[] {
     if (!value) {
-      throw new BadRequestException('Validation failed (string is expected)');
+      if (this.isOptional) {
+        return undefined;
+      }
+
+      Logger.error(`Error in ParseIntArrayPipe`);
+      throw new BadRequestException('Validation failed (string with numbers divided by commas is expected)');
     }
+
     const values = value.split(',').map(item => {
       return parseInt(item.trim(), 10);
     });
+
+    if (isNaN(values[values.length - 1])) {
+      Logger.error(`Error in ParseIntArrayPipe`);
+      throw new BadRequestException('Validation failed (unable to get numeric array of indexes)');
+    }
+
     return values;
   }
 }
