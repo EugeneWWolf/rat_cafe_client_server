68f29f2498b30b5457625ec334e9fcfecc4bd779
Cody <tortoisegames431@gmail.com>
2024-05-24 22:02:10 +0300

    Prohibit Body parameters which name is not in DTOs

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    forbidUnknownValues: true,
     transformOptions: { enableImplicitConversion: true },
   }));
 
